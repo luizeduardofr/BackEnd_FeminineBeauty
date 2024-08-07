01219514760ff67269080bf0de6a51fdd3946653
Luiz Eduardo F. <luizeduardo_francodarocha@outlook.com>
2024-08-06 21:56:29 -0300

    Atualizacao na entidade Funcionario

--- src/main/java/feminine_beauty/api/domain/funcionario/DadosCadastroFuncionario.java
+++ src/main/java/feminine_beauty/api/domain/funcionario/DadosCadastroFuncionario.java
@@ -1,4 +1,27 @@
 package feminine_beauty.api.domain.funcionario;
 
-public record DadosCadastroFuncionario() {
-}
+import feminine_beauty.api.domain.endereco.DadosEndereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record DadosCadastroFuncionario(
+
+        @NotBlank(message = "Nome é obrigatório.")
+        String nome,
+
+        @NotBlank(message = "Email é obrigatório.")
+        @Email(message = "Formato do email é inválido.")
+        String email,
+
+        @NotBlank(message = "Telefone é obrigatório.")
+        String telefone,
+
+        @NotBlank(message = "CPF é obrigatório.")
+        @CPF(message = "Formato do CPF é inválido.")
+        String cpf,
+
+        @NotNull(message = "Dados do endereço são obrigatórios")
+        @Valid DadosEndereco endereco) { }
--- src/main/java/feminine_beauty/api/domain/funcionario/Funcionario.java
+++ src/main/java/feminine_beauty/api/domain/funcionario/Funcionario.java
@@ -1,8 +1,19 @@
 package feminine_beauty.api.domain.funcionario;
 
 import feminine_beauty.api.domain.endereco.Endereco;
+import feminine_beauty.api.domain.servico.Servico;
 import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.List;
+
+@Table(name = "funcionarios")
+@Entity(name = "Funcionario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Funcionario {
 
     @Id
@@ -11,6 +22,11 @@ public class Funcionario {
     private String nome;
     private String email;
     private String telefone;
+    private String cpf;
+
+    @ManyToMany()
+    @JoinTable(joinColumns = @JoinColumn(name = "funcionario_id"), inverseJoinColumns = @JoinColumn(name = "servico_id"))
+    private List<Servico> listaServicos;
 
     @Embedded
     private Endereco endereco;
@@ -18,7 +34,7 @@ public class Funcionario {
     private Boolean ativo;
 
     public Funcionario (DadosCadastroFuncionario dados) {
-
+        this.nome = dados.nome();
     }
 
     public void atualizarInformacoes (DadosAtualizacaoFuncionario dados) {
