386aa9ecfaed5c10c89aed91ec1f156053ec4790
Luiz Eduardo F. <luizeduardo_francodarocha@outlook.com>
2024-09-26 17:29:00 -0300

    Alteracao mysql

--- src/main/resources/db/migration/V5__create-table-consultas.sql
+++ src/main/resources/db/migration/V5__create-table-consultas.sql
@@ -1,13 +1,13 @@
 CREATE TABLE consultas (
 
     id bigint not null auto_increment,
+    data datetime not null,
+    tipo_pagamento varchar(50) not null,
+    motivo_cancelamento varchar(50),
     funcionario_id bigint not null,
     cliente_id bigint not null,
-    data datetime not null,
-    tipoPagamento varchar(50) not null,
-    motivoCancelamento varchar(50),
 
     primary key(id),
     constraint fk_consultas_funcionario_id foreign key(funcionario_id) references funcionarios(id),
-    constraint fk_consultas_cliente_id foreign key(cliente_id) references clientes(id),
+    constraint fk_consultas_cliente_id foreign key(cliente_id) references clientes(id)
 );
