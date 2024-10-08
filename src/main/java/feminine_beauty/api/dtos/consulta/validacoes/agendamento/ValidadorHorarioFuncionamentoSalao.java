a1d9b1dc19a8678f5bf26c96e07462d3adf9cab7
Luiz Eduardo F. <109635035+luizeduardofr@users.noreply.github.com>
2024-10-08 13:16:00 -0300

    conversão de data

--- src/main/java/feminine_beauty/api/dtos/consulta/validacoes/agendamento/ValidadorHorarioFuncionamentoSalao.java
+++ src/main/java/feminine_beauty/api/dtos/consulta/validacoes/agendamento/ValidadorHorarioFuncionamentoSalao.java
@@ -5,16 +5,19 @@ import feminine_beauty.api.dtos.consulta.DadosAgendamentoConsulta;
 import org.springframework.stereotype.Component;
 
 import java.time.DayOfWeek;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 @Component()
 public class ValidadorHorarioFuncionamentoSalao implements ValidadorAgendamentoDeConsulta{
 
     public void validar(DadosAgendamentoConsulta dados){
-        var dataConsulta = dados.data();
+        var dataConsulta = dados.data().atZone(ZoneOffset.UTC);
 
         var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
-        var antesDaAberturaDoSalao = dataConsulta.getHour() < 7;
-        var depoisDoEncerramentoDoSalao = dataConsulta.getHour() > 18;
+        var antesDaAberturaDoSalao = dataConsulta.withZoneSameInstant(ZoneId.of("America/Sao_Paulo")).getHour() < 7;
+        var depoisDoEncerramentoDoSalao = dataConsulta.withZoneSameInstant(ZoneId.of("America/Sao_Paulo")).getHour() > 18;
         if(domingo || antesDaAberturaDoSalao || depoisDoEncerramentoDoSalao){
             throw new ValidacaoException("Consulta fora do horário de funcionamento do salão de beleza.");
         }
